e07ad7ac19196cf89a1a2cce7e2f5b0f9bc8ea01
Gaguchi <gaga9393@gmail.com>
2022-11-14 00:35:13 +0400

    newyear app working

--- lecture3/newyear/views.py
+++ lecture3/newyear/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import datetime
 # Create your views here.
+
+#HERE IS MY VERSION
+
+# def index(request):
+#     now = datetime.datetime.now()
+
+#     def is_it_ny(now):
+#         if now.month == 1 and now.day == 1:
+#             True
+#         else:
+#             False
+
+#     if is_it_ny == True:
+#         return HttpResponse("It's New Years!")
+#     else:
+#         return HttpResponse("It's not New Years!")
+
+#HERE IS THE LECTURE VERSION
+
+def index(request):
+    now = datetime.datetime.now()
+    return render(request, "newyear/index.html",{
+        "newyear": now.month == 1 and now.day ==1
+    })
