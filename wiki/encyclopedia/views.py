3c20e07b4110c5e2ce7a281cfd1df87f8934582f
Gaguchi <gaga9393@gmail.com>
2022-11-29 19:57:38 +0400

    search

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -10,16 +10,30 @@ class NewEntryForm(forms.Form):
     title = forms.CharField(label="title")
     content = forms.CharField(label="content")
 
+class SearchForm(forms.Form):
+    search = forms.CharField(label="search")
 
 def index(request):
+    if request.method == 'POST':
+        search = SearchForm(request.POST)
+        if search.is_valid():
+            search_result = search.cleaned_data["search"]
+            return HttpResponseRedirect(f'/{search_result}')
+        else:
+            return render(request, "encyclopedia/index.html", {
+                "entries": util.list_entries(),
+                "search": search
+            })
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "search":SearchForm()
     })
 
 def entry(request, title):
     return render(request, "encyclopedia/entry.html", {
         "entry" : util.get_entry(title),
-        "title": title
+        "title": title,
+        "search":SearchForm()
     })
 
 def new(request):
@@ -36,7 +50,8 @@ def new(request):
                 "form":form
             })
     return render(request, "encyclopedia/new.html",{
-        "form":NewEntryForm()
+        "form":NewEntryForm(),
+        "search":SearchForm()
     })
 
 def random_entry(request):
