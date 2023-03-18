fcf922f743620ac137030ef84a3dcd0cd91fa652
Gaguchi <gaga9393@gmail.com>
2023-03-18 22:02:08 +0400

    Admin classes

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -5,7 +5,9 @@ from .models import *
 class FlightAdmin(admin.ModelAdmin):
     list_display = ("id","origin","destination","duration")
 
-
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal =("flights",)
+    
 admin.site.register(Flight, FlightAdmin)
 admin.site.register(Airport)
-admin.site.register(Passenger)
+admin.site.register(Passenger, PassengerAdmin)
