9ff98d33a2b8587233bcd989e7fc8ab77b9264e8
Gaguchi <gaga9393@gmail.com>
2023-08-18 21:57:57 +0400

    working fetch and post

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -20,9 +20,36 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  document.querySelector('#send-email').addEventListener('click', () => {
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+          recipients: document.querySelector('#compose-recipients').value,
+          subject: document.querySelector('#compose-subject').value,
+          body: document.querySelector('#compose-body').value
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Print result
+        console.log(result);
+    });
+  });
+
 }
 
 function load_mailbox(mailbox) {
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+      // Print emails
+      console.log(emails);
+
+      // ... do something else with emails ...
+  });
+
+  console.log(mailbox)
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
